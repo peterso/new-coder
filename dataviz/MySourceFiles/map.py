7232e37f33e0233ee9b654de6e95ed9f85448adb
pso <petermjso@gmail.com>
2015-06-26 17:25:35 -0700

    successfully completed geojson tutorial. added notes on helpful virtualenvwrapper commands to read me file.

--- dataviz/MySourceFiles/map.py
+++ dataviz/MySourceFiles/map.py
@@ -1,3 +1,15 @@
+'''
+Data Visualization Project
+
+Parse data from an ugly CSV or Excel file, and render it in
+JSON-like form, visualize in graphs, and plot as a map.
+'''
+
+#from geojson import dumps
+import geojson
+
+import parse as p
+
 def create_map(data_file):
 	# Defile type of GeoJSON we're creating
 	geo_map = {"type": "FeatureCollection"}
@@ -8,19 +20,45 @@ def create_map(data_file):
 	# Iterate over our data to create GeoJSON document.
 	# We're using enumerate() so we get the line, as well 
 	# as the index, which is the line number.
+	for index, line in enumerate(data_file):
 
 		# Skip any zero coordinates as this will throw off our map.
+		if line['X'] == "0" or line['Y'] == "0": 
+			continue
 
 		# Setup a new dictionary for each iteration
+		data = {}
 
 		# Assign line items to appropriate GeoJSON fields.
+		data['type'] = 'Feature'
+		data['id'] = index
+		data['properties'] = {'title': line['Category'],
+							  'description': line['Descript'],
+							  'date': line['Date']}
+		data['geometry'] = {'type': 'Point',
+							 'coordinates': (line['X'], line['Y'])}
 
 		# Add data dictionary to our item_list
+		item_list.append(data)
 
 	# For each point in our item_list, we add the point to our dictionary.
 	# setdefault creates a key called 'features' that
 	# has a value tyoe of an empty list. With each iteration, we
 	# are appending our point to that list. 
+	for point in item_list:
+		geo_map.setdefault('features', []).append(point)
 
 	# now that all data is parsed in GeoJSON write to a file so we
 	# can upload it to gist.github.com
+	with open('file_sf.geojson', 'w') as f:
+		f.write(geojson.dumps(geo_map))
+
+
+def main():
+	data = p.parse(p.MY_FILE, ",")
+
+	return create_map(data)
+
+
+if __name__ == '__main__':
+	main()
--- dataviz/README.md
+++ dataviz/README.md
@@ -4,3 +4,16 @@
 To parse data from a CSV or Excel file and plot it with matplotlib. Examples include: parsing local crime data and visualizing how often crime happens on Mondays versus Thursdays, etc.
 
 Follow the complete tutorial [here](http://newcoder.io/dataviz)
+
+
+
+Here’s a run-down of useful commands for pip, virtualenv & virtualenvwrapper.
+
+    mkvirtualenv [ENV_NAME] – creates and activates a fresh virtual environment
+    workon [ENV_NAME] – activates an already-created virtual environment
+    deactivate – deactivates the virtual environment that is currently active
+    within an activated virtualenv, pip install [PACKAGE_NAME] installs a package into the virtualenv
+    within an activated virtualenv, pip freeze lists the packages that is installed & accessible within the virtualenv
+
+Here is a link to my map:
+https://gist.github.com/peterso/2831d243279e7caa1725
